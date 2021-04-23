8b6f3f9440cf0c974953c0527700dd642290d464
= <=>
2021-04-23 20:12:07 +0900

    aws sdk for node

--- ch05/03/ec2/ex02.js
+++ ch05/03/ec2/ex02.js
@@ -1,6 +1,5 @@
 // EC2 인스턴스 생성
 var AWS = require('aws-sdk');
-//AWS.config.loadFromPath('./config.json');
 
 ec2 = new AWS.EC2({
     region: 'ap-northeast-2',
@@ -8,23 +7,23 @@ ec2 = new AWS.EC2({
 });
 
 var params = {
-  ImageId: 'ami-018a9a930060d38aa',  // 필수
-  MaxCount: 1,  // 필수
-  MinCount: 1,  // 필수
-  Monitoring: { // 필수
-    Enabled: false
-  },
-  DryRun: false,
-//   IamInstanceProfile: {
-//     Name: 'ExampleEC2Role'
-//   },
-  InstanceType: 't2.micro',
-  KeyName: 'mykey'
+    ImageId: 'ami-018a9a930060d38aa',  // 필수
+    MaxCount: 1,  // 필수
+    MinCount: 1,  // 필수
+    Monitoring: { // 필수
+        Enabled: false
+    },
+    DryRun: false,
+    InstanceType: 't2.micro',
+    KeyName: 'mykey'
 };
 
-ec2.runInstances(params, function (err, data) {
-  if (err)
-    console.log(err, err.stack);
-  else
+ec2.runInstances(params, function(err, data) {
+    if(err){
+        console.log(err, err.stack);
+        return;
+    } 
+
     console.log(data);
 });
+
