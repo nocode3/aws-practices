e5faf74c1e63df7e3621dcfb440205084d0426fd
= <=>
2021-04-24 07:55:36 +0900

    myapp-setup.sh

--- ch05/04/myapp-setup.sh
+++ ch05/04/myapp-setup.sh
@@ -2,3 +2,6 @@
 sudo yum install git -y
 sudo curl --silent --location https://rpm.nodesource.com/setup_7.x|bash -
 sudo yum -y install nodejs
+
+git clone https://github.com/kickscar/aws-practices.git
+node aws-practices/ch05/04/myapp/index.js &
