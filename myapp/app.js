cde9eb754d1d48c1b8751e406911a3194d9c3318
kickscar <kickscar@gmail.com>
2021-04-27 19:12:52 +0900

    myapp 버그 수정

--- myapp/app.js
+++ myapp/app.js
@@ -57,5 +57,5 @@ function onError(error) {
 function onListening() {
     var addr = server.address();
     var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-    console.debug('Listening on ' + bind);
+    console.log('Listening on ' + bind);
 }
