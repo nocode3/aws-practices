6661af71650cc8cd6a5f5e96af99cec9dc1519c0
kickscar <kickscar@gmail.com>
2021-05-05 13:09:39 +0900

    setup.sh 수정

--- ch04/04/setup.sh
+++ ch04/04/setup.sh
@@ -3,7 +3,7 @@ sudo yum install git -y
 sudo curl --silent --location https://rpm.nodesource.com/setup_7.x | bash -
 sudo yum -y install nodejs
 
-git clone https://github.com/aws-bitacademy/myapp2.git
+git clone https://github.com/kickscar/myapp2.git
 cd /myapp2
 
 npm install
